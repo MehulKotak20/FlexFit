1f29f4faa595942a20a0a5d5cbc1e32c5a4a41e1
meetkavathiya <meetpatel76413333@gmail.com>
2025-03-07 10:43:42 +0530

    db

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,10 @@ import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import path from "path";
-import passport from "passport";
-import session from "express-session";
+
 import { connectDB } from "./db/connectDB.js";
 
-import authRoutes from "./routes/auth.route.js";
+
 
 dotenv.config();
 
@@ -16,23 +15,19 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json()); 
+app.use(cookieParser()); 
 
-app.use(express.json()); // allows us to parse incoming requests:req.body
-app.use(cookieParser()); // allows us to parse incoming cookies
-
-app.use("/api/auth", authRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
-
-
 app.listen(PORT, () => {
-	connectDB();
-	console.log("Server is running on port: ", PORT);
+  connectDB();
+  console.log("Server is running on port: ", PORT);
 });
